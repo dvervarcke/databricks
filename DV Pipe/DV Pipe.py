b48d214a3affd9bcb1d8905c2ac9a8ed8816df40
dvervarcke <75751299+dvervarcke@users.noreply.github.com>
2026-02-17 11:36:14 -0800

    Delete DV Pipe directory

--- DV Pipe/DV Pipe.py
+++
@@ -1,5 +0,0 @@
-# Databricks notebook source part 2
-query = "SELECT * FROM population_data WHERE country = 'Belgium'"
-df = spark.sql(query)
-pandas_df = df.toPandas()  # Convert to pandas DataFrame
-display(pandas_df)
