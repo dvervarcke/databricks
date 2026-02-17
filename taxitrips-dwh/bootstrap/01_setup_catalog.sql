5efdd2fba842eb0c689509c4ed7c7185436762a1
dvervarcke <75751299+dvervarcke@users.noreply.github.com>
2026-02-17 11:37:33 -0800

    Delete taxitrips-dwh directory

--- taxitrips-dwh/bootstrap/01_setup_catalog.sql
+++
@@ -1,9 +0,0 @@
-CREATE CATALOG IF NOT EXISTS taxi_dwh;
-
-CREATE SCHEMA IF NOT EXISTS taxi_dwh.bronze;
-CREATE SCHEMA IF NOT EXISTS taxi_dwh.silver;
-CREATE SCHEMA IF NOT EXISTS taxi_dwh.gold;
-
-COMMENT ON SCHEMA taxi_dwh.bronze IS 'Raw taxi trip ingestion layer';
-COMMENT ON SCHEMA taxi_dwh.silver IS 'Cleaned and conformed taxi trips';
-COMMENT ON SCHEMA taxi_dwh.gold IS 'Analytics-ready taxi marts';
